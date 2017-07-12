9fa09223cd3e18db83969c3ebbd1392f67ceb9a0
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-07-11 21:21:26 -0500

    added stats.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,20 @@
+// stats.js
+var stats = new Stats();
+stats.showPanel( 1 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild( stats.dom );
+
+function animate() {
+
+	stats.begin();
+	stats.end();
+
+	requestAnimationFrame( animate );
+
+}
+
+requestAnimationFrame( animate );
+
+
 var example = (function(){
   "use strict";
 
@@ -50,3 +67,7 @@ var example = (function(){
     scene: scene
   }
 })();
+
+function degreesToRadians(degree){
+  return degree * (3.14159 / 180)
+}
